30ca89a06335b5378462c8e6089800ef0b479446
koobe <koobe.liu@uaf.com.cn>
2023-03-13 09:49:09 +0800

    update

--- src/model/db.rs
+++ src/model/db.rs
@@ -2,6 +2,6 @@ use rbatis::rbatis::Rbatis;
 
 pub async fn init_db() -> Rbatis {
     let rb = Rbatis::new();
-    rb.init(rbdc_mysql::driver::MysqlDriver {}, "mysql://root:r-wz9wop62956dh5k9ed@rm-wz9a2yv489d123yqkdo.mysql.rds.aliyuncs.com:3306/rustdb").unwrap();
+    rb.init(rbdc_mysql::driver::MysqlDriver {}, "mysql://root:123456@127.0.0.1:3306/rustdb").unwrap();
     return rb;
 }
