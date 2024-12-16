3ba4095f3ad44a8715f56431eb4fa253a83e0d78
liufeihua <1002219331@qq.com>
2024-12-16 16:42:19 +0800

    脚本调整

---
+++ docs/sql/system/sys_role_menu.sql
@@ -0,0 +1,9 @@
+create table sys_role_menu
+(
+    id          bigint auto_increment comment '主键'
+        primary key,
+    role_id     bigint                             not null comment '角色ID',
+    menu_id     bigint                             not null comment '菜单ID',
+    create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+)
+    comment '菜单角色关联表';
---
+++ docs/sql/system/sys_user_role.sql
@@ -0,0 +1,9 @@
+create table sys_user_role
+(
+    id          bigint auto_increment comment '主键'
+        primary key,
+    user_id     bigint   default 0                 not null comment '用户ID',
+    role_id     bigint                             not null comment '角色ID',
+    create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+)
+    comment '角色用户关联表';
