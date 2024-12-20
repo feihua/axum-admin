a5a83c64c571581e55f77e31f733680377eca74c
liufeihua <1002219331@qq.com>
2024-12-20 16:36:02 +0800

    sql

--- docs/sql/system/sys_user_role.sql
+++ docs/sql/system/sys_user_role.sql
@@ -7,4 +7,4 @@ create table sys_user_role
     create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间'
 )
     comment '角色用户关联表';
-INSERT INTO rustdb.sys_user_role (user_id, role_id) VALUES (1, 1)
+INSERT INTO sys_user_role (user_id, role_id) VALUES (1, 1)
