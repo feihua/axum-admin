7ad1b6bdf33939d8c6ee1a8be0f533b8bc32f944
liufeihua <1002219331@qq.com>
2025-03-12 10:25:32 +0800

    feat(config): implement configuration file loading and database initialization
    - Add configuration file loading functionality using the `config` crate- Refactor `init_db` function to accept a database URL parameter
    - Update `main` function to use the configuration file for server and database settings- Improve error handling and logging in the database initialization process

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,61 @@ use crate::route::system::sys_login_log_route::build_sys_login_log_route;
 use crate::route::system::sys_notice_route::build_sys_notice_route;
 use crate::route::system::sys_operate_log_route::build_sys_operate_log_route;
 use crate::route::system::sys_post_route::build_sys_post_route;
+use config::{Config, File};
 use middleware::auth::auth;
 use rbatis::RBatis;
 use route::system::sys_menu_route::build_sys_menu_route;
 use route::system::sys_role_route::build_sys_role_route;
 use route::system::sys_user_route::build_sys_user_route;
+use serde::Deserialize;
 use std::sync::Arc;
 
+// 定义应用状态结构体，包含数据库连接池
 pub struct AppState {
     pub batis: RBatis,
 }
 
+// 配置结构体，包含服务器和数据库配置
+#[derive(Debug, Deserialize)]
+struct Config1 {
+    server: ServerConfig,
+    db: DbConfig,
+}
+
+// 服务器配置结构体，包含服务器地址
+#[derive(Debug, Deserialize)]
+struct ServerConfig {
+    addr: String,
+}
+
+// 数据库配置结构体，包含数据库URL
+#[derive(Debug, Deserialize)]
+struct DbConfig {
+    url: String,
+}
+
+// 主函数，使用tokio异步运行时
 #[tokio::main]
 async fn main() {
+    // 初始化日志配置
     log4rs::init_file("src/config/log4rs.yaml", Default::default()).unwrap();
-    let rb = init_db().await;
-    let shared_state = Arc::new(AppState {
-        /* ... */ batis: rb.clone(),
-    });
 
+    // 加载和解析配置文件
+    let config = Config::builder()
+        .add_source(File::with_name("config.toml"))
+        .build()
+        .unwrap()
+        .try_deserialize::<Config1>()
+        .unwrap();
+    println!("Config: {:?}", config);
+
+    // 初始化数据库连接
+    let rb = init_db(config.db.url.as_str()).await;
+
+    // 创建共享应用状态，包含数据库连接池
+    let shared_state = Arc::new(AppState { batis: rb.clone() });
+
+    // 构建应用路由，并合并多个子路由
     let app = Router::new().nest(
         "/api",
         Router::new()
@@ -51,19 +87,25 @@ async fn main() {
             .merge(build_sys_login_log_route())
             .merge(build_sys_operate_log_route())
             .merge(build_sys_notice_route())
-            .route_layer(md::from_fn(auth))
-            .with_state(shared_state),
+            .route_layer(md::from_fn(auth)) // 添加认证中间件
+            .with_state(shared_state), // 设置共享状态
     );
 
-    // axum 0.6.x
+    // 以下代码适用于axum 0.6.x版本
+    // 定义服务器监听地址
     // let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     // log::info!("listening on {}", addr);
+    // 使用绑定地址启动服务器
     // axum::Server::bind(&addr)
     //     .serve(app.into_make_service())
     //     .await
     //     .unwrap();
 
-    // axum 0.7.x
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // 以下代码适用于axum 0.7.x版本
+    // 创建TCP监听器
+    let listener = tokio::net::TcpListener::bind(config.server.addr)
+        .await
+        .unwrap();
+    // 使用监听器启动服务器
     axum::serve(listener, app).await.unwrap();
 }
--- src/model/db.rs
+++ src/model/db.rs
@@ -1,11 +1,24 @@
 use rbatis::rbatis::RBatis;
 
-pub async fn init_db() -> RBatis {
+/// 初始化数据库连接
+///
+/// # Parameters
+///
+/// * `url`: &str - 数据库的连接URL
+///
+/// # Returns
+///
+/// * `RBatis` - 初始化后的RBatis实例
+///
+/// # Panics
+///
+/// 当数据库连接初始化失败时，该函数会通过`expect`方法抛出panic
+pub async fn init_db(url: &str) -> RBatis {
+    // 创建一个新的RBatis实例
     let rb = RBatis::new();
-    rb.init(
-        rbdc_mysql::driver::MysqlDriver {},
-        "mysql://root:123456@127.0.0.1:3306/rustdb",
-    )
-    .unwrap();
-    return rb;
+    // 使用提供的URL和MysqlDriver初始化RBatis实例
+    rb.init(rbdc_mysql::driver::MysqlDriver {}, url)
+        .expect("init db error");
+    // 返回初始化后的RBatis实例
+    rb
 }
