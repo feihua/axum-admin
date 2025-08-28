1ea9cd443637f6ac51e3c1904095f38b5db77762
liufeihua <1002219331@qq.com>
2025-08-28 13:47:31 +0800

    build(rbdc): update database connection pool implementation

--- src/utils/db.rs
+++ src/utils/db.rs
@@ -1,24 +1,14 @@
 use rbatis::rbatis::RBatis;
+use rbatis::rbdc::pool::{ConnectionManager, Pool};
+use rbdc_mysql::MysqlDriver;
+use rbdc_pool_fast::FastPool;
 
-/// 初始化数据库连接
-///
-/// # Parameters
-///
-/// * `url`: &str - 数据库的连接URL
-///
-/// # Returns
-///
-/// * `RBatis` - 初始化后的RBatis实例
-///
-/// # Panics
-///
-/// 当数据库连接初始化失败时，该函数会通过`expect`方法抛出panic
 pub async fn init_db(url: &str) -> RBatis {
-    // 创建一个新的RBatis实例
     let rb = RBatis::new();
-    // 使用提供的URL和MysqlDriver初始化RBatis实例
-    rb.init(rbdc_mysql::driver::MysqlDriver {}, url)
-        .expect("init db error");
-    // 返回初始化后的RBatis实例
+
+    let manager = ConnectionManager::new(MysqlDriver {}, url).expect("create connection manager error");
+    let pool = FastPool::new(manager).expect("create db pool error");
+
+    rb.init_pool(pool).expect("init db pool error");
     rb
 }
